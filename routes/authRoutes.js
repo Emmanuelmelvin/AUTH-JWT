415ce4539ef573ad71f0d55d3f1904d640e9e8fa
Emmanuelmelvin <chidiemmanuel2005@gmail.com>
2024-07-16 19:10:42 -0700

    Created routes and controllers

---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = eouter = express.Router()
+
+router.get('/signup',  (req, res) => {
+    
+})
+
+router.get('/signup',  (req, res) => {
+    
+})
+
+module.exports = router;
